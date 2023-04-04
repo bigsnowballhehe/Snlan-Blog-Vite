75906ca075debd51443d99bad4ec395ba10cb76b
Snlan <pridewui@foxmail.com>
2023-04-04 16:17:42 +0800

    feat:add githubpage

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base:'https://bigsnowballhehe.github.io/',
   title: "SnLan's blog",
   description: "A VitePress Site",
   head:[
